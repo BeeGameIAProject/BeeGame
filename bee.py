a50ae6a61b714fc8c368f20b5cbc1ece31421baf
Jose Candon Rubio <jcandoru7@alumnes.ub.edu>
2025-11-24 21:24:35 +0100

    - MPV - Traducción al Castellano

--- bee.py
+++ bee.py
@@ -10,7 +10,7 @@ class Bee():
         self.max_vida = life
     
     def to_string(self):
-        return f"Life: {self.life}, Juga primer: {self.first_move}, Icona: {self.name}, Nom jugador: {self.player_name}"
+        return f"Life: {self.life}, Juega primero: {self.first_move}, Icono: {self.name}, Nombre jugador: {self.player_name}"
     
     def is_valid_move(self,board,start,to):
         """Comprueba si el movimiento es válido (estilo rey de ajedrez)."""
@@ -19,47 +19,46 @@ class Bee():
 
         # Comprobar límites
         if not (0 <= to[0] < filas and 0 <= to[1] < columnas):
-            print("Moviment invàlid: fora del tauler.")
+            print("Movimiento inválido: fuera del tablero.")
             return False
 
         if abs(start[0] - to[0]) <= 1 and abs(start[1] - to[1]) <= 1:
             return True
 
-        print("Moviment invàlid.")
+        print("Movimiento inválido.")
         return False
 
-    def abella_tocada(self):
-        """Baja la vida con daño predeteminado"""
-        self.life -=self.daño_ataque
+    def abeja_tocada(self):
+        """Baja la vida con daño predeterminado"""
+        self.life -= self.daño_ataque
         if self.life <0:
             self.life = 0
         
-    def baixar_vida(self,ataque):
-        """Baja la vida con daño especifico"""
-        self.life -=ataque
+    def bajar_vida(self, ataque):
+        """Baja la vida con daño específico"""
+        self.life -= ataque
         if self.life <0:
             self.life = 0
         
-    def pujar_vida(self,cura):
-        """Puja la vida"""
-        
+    def subir_vida(self, cura):
+        """Sube la vida"""
         self.life += cura
         if self.life > self.max_vida:
             self.life = self.max_vida 
     
     def next_moves(self, board, position):
-        """Retorna els seguents moviments posibles"""
+        """Retorna los siguientes movimientos posibles"""
         m = []
         filas = board.fila()
         columnas = board.columna()
         fila, col = position
 
-        # Movimientos posibles 
-        direcciones = [(-1, -1), (-1, 0), (-1, 1),( 0, -1),( 0, 1),( 1, -1), ( 1, 0), ( 1, 1)]
+        # Movimientos posibles
+        direcciones = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
         m = [
-        (fila + df, col + dc)
-        for df, dc in direcciones
-        if 0 <= fila + df < filas and 0 <= col + dc < columnas
+            (fila + df, col + dc)
+            for df, dc in direcciones
+            if 0 <= fila + df < filas and 0 <= col + dc < columnas
         ]
         return m    
     def esta_viva(self):
